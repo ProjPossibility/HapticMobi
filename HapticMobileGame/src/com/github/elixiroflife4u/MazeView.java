e01a584beb1a210b80a13705ba63c5a78942fb05
Tarun Solanki <elixiroflife4u@gmail.com>
2012-01-15 11:49:25 -0800

    got accelerometer working. Also fixed the graphics issue where redraw wasn't being called in an synchronous fashion.

--- HapticMobileGame/src/com/github/elixiroflife4u/MazeView.java
+++ HapticMobileGame/src/com/github/elixiroflife4u/MazeView.java
@@ -1,6 +1,7 @@
 package com.github.elixiroflife4u;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.view.View;
@@ -30,6 +31,10 @@ public class MazeView extends View {
 	private Dir mBallCellOffset = Dir.NONE;
 	// ball foreground color
 	private int mBallColor = 0xFF00FFFF;
+	// pre-rendered BG and ball bitmaps
+	Bitmap mBGBitmap = null, mBallBitmap = null;
+	// ball velocity
+	float mBallVX, mBallVY;
 	
 	MazeView(Context ctx) {
 		super(ctx);
@@ -39,12 +44,15 @@ public class MazeView extends View {
 		// cells are always square -> find the max cell size that will fit
 		float cellw = (float)w / mNumCellsX;
 		float cellh = (float)h / mNumCellsY;
-		mCellSize = Math.min(cellw, cellh);
+		mCellSize = (float) Math.floor(Math.min(cellw, cellh));
 		
 		mGridOriginX = 0.5f * (w - mCellSize * mNumCellsX);
 		mGridOriginY = 0.5f * (h - mCellSize * mNumCellsY);
 		
 		setBallPosition(mBallCellX, mBallCellY);
+		
+		mBallBitmap = null;
+		mBGBitmap = null;
 	}
 	
 	public void setGridDim(int cellsAcross, int cellsDown) {
@@ -269,50 +277,107 @@ public class MazeView extends View {
 		recomputeBallCellY();
 	}
 	
+	public void ballDeltaV(float dvx, float dvy)
+	{
+		final float thresh = 1.1f;
+		if (Math.abs(dvx) > thresh || Math.signum(dvx) != Math.signum(mBallVX))
+			mBallVX += dvx;
+		if (Math.abs(dvy) > thresh || Math.signum(dvy) != Math.signum(mBallVY))
+			mBallVY += dvy;
+		
+		final float vmax = 2.f;
+		if (mBallVX > vmax)
+			mBallVX = vmax;
+		else if (mBallVX < -vmax)
+			mBallVX = -vmax;
+		if (mBallVY > vmax)
+			mBallVY = vmax;
+		else if (mBallVY < -vmax)
+			mBallVY = -vmax;
+		
+		invalidate();
+	}
+	
+	private void renderMazeBitmaps()
+	{
+		int cellsize = (int) Math.round(mCellSize);
+		mBGBitmap = Bitmap.createBitmap(mNumCellsX*cellsize, mNumCellsY*cellsize, Bitmap.Config.ARGB_8888);
+		Canvas bgCanvas = new Canvas(mBGBitmap);
+		
+		// draw maze background (may not fill entire screen)
+		Paint paint = new Paint();
+		paint.setColor(mBGColor);
+		bgCanvas.drawRect(0, 0, mCellSize*mNumCellsX, mCellSize*mNumCellsY, paint);
+
+		// draw cell walls
+		int ix, iy;
+		float left, top, right, btm;
+		float halfWallThickness = 0.5f * mWallThickness;
+		paint.setColor(mWallColor);
+		
+		for (ix = 0; ix < mNumCellsX; ix++) {
+			for (iy = 0; iy < mNumCellsY; iy++) {
+				float x = mCellSize * ix;
+				float y = mCellSize * iy;
+				Cell c = mCells[iy][ix];
+				if (iy > 0 && c.northWall) {
+					// horizontal wall
+					left = x;
+					right = x + mCellSize;
+					top = y - halfWallThickness;
+					btm = y + halfWallThickness;
+					bgCanvas.drawRect(left, top, right, btm, paint);
+				}
+				if (ix > 0 && c.westWall) {
+					// vertical wall
+					top = y;
+					btm = y + mCellSize;
+					left = x - halfWallThickness;
+					right = x + halfWallThickness;
+					bgCanvas.drawRect(left, top, right, btm, paint);
+				}
+			}
+		}
+		
+		// draw ball
+		mBallBitmap = Bitmap.createBitmap(cellsize, cellsize, Bitmap.Config.ARGB_8888);
+		Canvas ballCanvas = new Canvas(mBallBitmap);
+		paint.setAntiAlias(true);
+		paint.setColor(mBallColor);				// 0.45 instead of 0.5 to be slightly smaller than cell
+		float radius = 0.5f*(float)cellsize;
+		ballCanvas.drawCircle(radius, radius, 0.45f * mCellSize, paint);
+	}
+	
+	private long mLastDrawTime = -1;
+	
 	@Override
 	protected void onDraw(Canvas canvas) {
 
 		if (!magnify)
 		{
-			// draw maze background (may not fill entire screen)
+			if (mBGBitmap== null || mBallBitmap == null)
+				renderMazeBitmaps();
+			
+			float radius = 0.5f*mCellSize;
 			Paint paint = new Paint();
-			paint.setColor(mBGColor);
-			canvas.drawRect(mGridOriginX, mGridOriginY, mGridOriginX+mCellSize*mNumCellsX, mGridOriginY+mCellSize*mNumCellsY, paint);
-
-			// draw cell walls
-			int ix, iy;
-			float left, top, right, btm;
-			float halfWallThickness = 0.5f * mWallThickness;
-			paint.setColor(mWallColor);
+			canvas.drawBitmap(mBGBitmap, mGridOriginX, mGridOriginY, paint);
+			canvas.drawBitmap(mBallBitmap, mGridOriginX+mBallX-radius, mGridOriginY+mBallY-radius, paint);
 			
-			for (ix = 0; ix < mNumCellsX; ix++) {
-				for (iy = 0; iy < mNumCellsY; iy++) {
-					float x = mGridOriginX + mCellSize * ix;
-					float y = mGridOriginY + mCellSize * iy;
-					Cell c = mCells[iy][ix];
-					if (iy > 0 && c.northWall) {
-						// horizontal wall
-						left = x;
-						right = x + mCellSize;
-						top = y - halfWallThickness;
-						btm = y + halfWallThickness;
-						canvas.drawRect(left, top, right, btm, paint);
-					}
-					if (ix > 0 && c.westWall) {
-						// vertical wall
-						top = y;
-						btm = y + mCellSize;
-						left = x - halfWallThickness;
-						right = x + halfWallThickness;
-						canvas.drawRect(left, top, right, btm, paint);
-					}
-				}
-			}
+			long curTime = System.currentTimeMillis();
+			float dt = (mLastDrawTime < 0) ? 5.f : (float)(curTime - mLastDrawTime);
+			mLastDrawTime = curTime;
+			// scale dt to a reasonable value
+			dt *= 0.1f;
 			
-			// draw ball
-			paint.setAntiAlias(true);
-			paint.setColor(mBallColor);				// 0.45 instead of 0.5 to be slightly smaller than cell
-			canvas.drawCircle(mGridOriginX+mBallX, mGridOriginY+mBallY, 0.45f * mCellSize, paint);
+			// update ball position based on velocity
+			if (mBallVX > 0.f)
+				shiftBallRight(mBallVX*dt);
+			else if (mBallVX < 0.f)
+				shiftBallLeft(-mBallVX*dt);
+			if (mBallVY > 0.f)
+				shiftBallDown(mBallVY*dt);
+			else if (mBallVY < 0.f)
+				shiftBallUp(-mBallVY*dt);
 		}
 		else
 		{
